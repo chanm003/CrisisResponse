5fd2276a9d2cc0d5099b450833a449568940ef89
Mike Chan <mike.chan@kmdev.local>
2017-01-09 11:09:22 +0100

    Removed option from MultiChoice field

--- contentGeneration/public/src/app/schema/schema.js
+++ contentGeneration/public/src/app/schema/schema.js
@@ -1536,7 +1536,7 @@ crisisResponseSchema.listDefs["Calendar"] = {
 			Required: "FALSE",
 			FillInChoice: "FALSE",
 			Description: "Select all that apply",
-			Choices: ['Battle Rhythm', 'Working Group', 'Briefing', 'SOCEUR Internal', 'Reporting Requirement', 'Academics', 'VTC'],
+			Choices: ['Battle Rhythm', 'Working Group', 'Briefing', 'Reporting Requirement', 'Academics', 'VTC'],
 			Default: ''						//(optional)
 		}
 	],	
